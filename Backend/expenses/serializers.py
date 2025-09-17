ce0b1a05e4fb9bcbf8a7e33b72349628e8564227
HimalBhandari05 <himalbhandari342@gmail.com>
2025-09-17 13:45:19 +0545

    feat: LoginView , RegisterView created

--- Backend/expenses/serializers.py
+++ Backend/expenses/serializers.py
@@ -3,6 +3,33 @@ from rest_framework import serializers
 from .models import Expenses, Categories
 from django.contrib.auth.models import User
 
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only =True)
+    class Meta:
+        model = User
+        fields = ['username' , 'password' , 'password2' , 'email'] # yesle chai register garna ko laagi serializer aafei banauxa hai ta.
+        
+    def validate(self , data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Password must match")
+        return data
+
+    def create(self , validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email = validated_data['email']
+        )
+        return user
+
+
+
+
+
+
+
 class CategorySerializer(serializers.ModelSerializer): # how this works is explained in views.py 
     class Meta:
         model = Categories
@@ -11,10 +38,9 @@ class CategorySerializer(serializers.ModelSerializer): # how this works is expla
     
 class ExpenseSerializer(serializers.ModelSerializer): # yesle chai expense model lai serialize garxa 
     category = CategorySerializer(read_only=True) # nested serializer to include category details eg. of nested {"category": {"id": 1, "name": "Food"}}
-
     category_id = serializers.PrimaryKeyRelatedField(
         queryset=Categories.objects.all(), source='category', write_only=True, required=False
-    ) 
+    )
     
     user = serializers.StringRelatedField(read_only=True) # to show username instead of user id in serialized output
     
