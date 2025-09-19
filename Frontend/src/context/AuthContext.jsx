0245612bf32d5d71cc69909d19ff4216e78d7dc1
HimalBhandari05 <himalbhandari342@gmail.com>
2025-09-19 20:03:58 +0545

    feat: authentication context api which handles login/logout/loading

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -16,11 +16,11 @@ function decodeJwt(token) {
 
   try {
     const payload = token.split(".")[1];
-    const payLoadDecoded = atob(payloadBase64);
+    const json = JSON.parse(
+      atob(payload.replace(/-/g, "+").replace(/_/g, "/"))
+    );
 
-    const payloadObj = JSON.parse(payLoadDecoded);
-
-    console.log(payloadObj);
+    console.log(json);
 
     return json;
   } catch {
@@ -30,5 +30,46 @@ function decodeJwt(token) {
 
 export const AuthProvider = (props) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // loading = true -> app is checking if the user is logged in or not. else not checking
+
+  useEffect(() => {
+    const access = localStorage.getItem("access_token");
+    if (access) {
+      const payload = decodeJwt(access);
+      if (payload && payload.exp * 1000 > Date.now()) {
+        setUser(payload);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+      }
+    } else {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+    }
+    setLoading(false);
+  }, []);
+
+  const login = ({ access, refresh }) => {
+    localStorage.setItem("access_token", access);
+    localStorage.setItem("refresh_token", refresh);
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+    setUser(decodeJwt(access));
+  };
+
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    delete axios.defaults.headers.common["Authorization"];
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, loading, login, logout, isAuthenticated: !!user }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
+
+
+export const useAuth = ()=> useContext(AuthContext); // yesle context lai sajilo use garna help garxa
