33e9a99a13fec4e1852d1a311bc61f9dc6534221
HimalBhandari05 <himalbhandari342@gmail.com>
2025-09-27 11:55:51 +0545

    added categories and expenses context

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ExpenseProvider } from "./context/expenseContext.jsx";
+import { CategoryProvider } from "./context/categoryContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <ExpenseProvider>
-      <App />
-    </ExpenseProvider>
+    <CategoryProvider>
+      <ExpenseProvider>
+        <App />
+      </ExpenseProvider>
+    </CategoryProvider>
   </AuthProvider>
 );
