2634eff92ee0ea556e3f715c1ae5c446e9367845
HimalBhandari05 <himalbhandari342@gmail.com>
2025-10-03 20:09:07 +0545

    ..

--- Frontend/src/components/Report.jsx
+++ Frontend/src/components/Report.jsx
@@ -1,9 +1,25 @@
 import { Chart as ChartJS } from "chart.js/auto"
+import { useState } from "react"
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
 
-function Report() {
+ChartJS.register(
+    CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend
+)
+
+
+function Report() { 
     return (
         <>
-            
+
         </>
     )
 }
